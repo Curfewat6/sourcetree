1b9ff3f7460c814e57d6c72b73530351483433e3
RT <ryantay32@gmail.com>
2024-02-18 17:46:12 +0800

    Merge issue resolved

--- desktop/src/com/mygdx/game/EntityManagement.java
+++ desktop/src/com/mygdx/game/EntityManagement.java
@@ -8,8 +8,7 @@ public class EntityManagement implements EntityManager{
 	
 	public EntityManagement() {
         entities = new ArrayList<>();
-    }
-
+    }    
     public void addEntity(Entity entity) {
         entities.add(entity);
     }
@@ -38,5 +37,23 @@ public class EntityManagement implements EntityManager{
             entity.dispose();
         }
         entities.clear();
+    }
+    
+    public void checkCollide() {
+    	for (int i = 0; i < entities.size(); i++) {
+    		for (int j = i + 1; j < entities.size() ; j++) {
+    			Entity a = entities.get(i);
+    			Entity b = entities.get(j);
+    			
+    			if (a instanceof TextureObject && b instanceof TextureObject) {
+    				TextureObject A = (TextureObject) a;
+    				TextureObject B = (TextureObject) b;
+
+    				if(A.collide(B)) {
+    					System.out.println("BANG!!!");
+    				}
+    			}
+    		}
+    	}
     }
 }
--- desktop/src/com/mygdx/game/EntityManager.java
+++ desktop/src/com/mygdx/game/EntityManager.java
@@ -7,56 +7,6 @@ public interface EntityManager {
 	public abstract void move();
 	public abstract void dispose();
 	public abstract void update();
+	public abstract void checkCollide();
 	
-
-<<<<<<< HEAD
-=======
-    public void addEntity(Entity entity) {
-        entities.add(entity);
-    }
-
-    public void draw() {
-        for (Entity entity : entities) {
-        	entity.create();
-            entity.render(); 
-        }
-    }
-
-    public void move() {
-        for (Entity entity : entities) {
-            entity.move(); 
-        }
-    }
-
-    public void update() {
-        for (Entity entity : entities) {
-            entity.update();
-        }
-    }
-
-    public void dispose() {
-        for (Entity entity : entities) {
-            entity.dispose();
-        }
-        entities.clear();
-    }
-    
-    public void checkCollide() {
-    	for (int i = 0; i < entities.size(); i++) {
-    		for (int j = i + 1; j < entities.size() ; j++) {
-    			Entity a = entities.get(i);
-    			Entity b = entities.get(j);
-    			
-    			if (a instanceof TextureObject && b instanceof TextureObject) {
-    				TextureObject A = (TextureObject) a;
-    				TextureObject B = (TextureObject) b;
-
-    				if(A.collide(B)) {
-    					System.out.println("BANG!!!");
-    				}
-    			}
-    		}
-    	}
-    }
->>>>>>> 01816785c0799b39be7960dc32d04fffb34e2038
 }
