833004220ea229f84fe1d787a854edb95c31b0c5
Curfewat6 <75602956+Curfewat6@users.noreply.github.com>
2024-02-19 23:19:40 +0800

    Update PlayerControlManager.java

--- desktop/src/com/mygdx/game/pcm/PlayerControlManager.java
+++ desktop/src/com/mygdx/game/pcm/PlayerControlManager.java
@@ -2,6 +2,7 @@ package com.mygdx.game.pcm;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.utils.Null;
 import com.mygdx.game.entity.Circle;
 import com.mygdx.game.entity.EntityManager;
 import com.mygdx.game.entity.TextureObject;
@@ -9,61 +10,82 @@ import com.mygdx.game.entity.Triangle;
 
 public class PlayerControlManager implements PlayerControlManagement {
     private EntityManager entityManager;
+    private String direction;
 
     public PlayerControlManager(EntityManager entityManager) {
         this.entityManager = entityManager;
+        this.direction = null;
     }
 
-    public void handlingPlayerInput(String direction) {
+    // This method is to set direction so the object knows which way to go based on what key
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+
+    public void handlingPlayerInput() {
         entityManager.getEntities().forEach(entity -> {
             if (entity instanceof TextureObject) {
                 TextureObject textureObject = (TextureObject) entity;
                 if (textureObject.getName().equals("bucket.png")) {
-                    handleBucketInput(textureObject, direction);
+                    handleBucketInput(textureObject);
                 }
             } else if (entity instanceof Circle) {
                 Circle circle = (Circle) entity;
-                handleCircleInput(circle, direction);
+                handleCircleInput(circle);
             } else if (entity instanceof Triangle) {
                 Triangle triangle = (Triangle) entity;
-                handleTriangleInput(triangle, direction);
+                handleTriangleInput(triangle);
             }
         });
     }
 
-    public void handleBucketInput(TextureObject bucket, String direction) {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+    //After moving then i set direction to null so it won't drift but idk if there is a cleaner way to do this :-(
+    public void handleBucketInput(TextureObject bucket) {
+        if (this.direction == "arrow-left") {
             bucket.setPosX(bucket.getPosX() - (bucket.getSpeed() * Gdx.graphics.getDeltaTime()));
+            this.direction = null;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        if (this.direction == "arrow-right") {
             bucket.setPosX(bucket.getPosX() + (bucket.getSpeed() * Gdx.graphics.getDeltaTime()));
+            this.direction = null;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        if (this.direction == "arrow-up") {
             bucket.setPosY(bucket.getPosY() + (bucket.getSpeed() * Gdx.graphics.getDeltaTime()));
+            this.direction = null;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+        if (this.direction == "arrow-down") {
             bucket.setPosY(bucket.getPosY() - (bucket.getSpeed() * Gdx.graphics.getDeltaTime()));
+            this.direction = null;
         }
+        
     }
     
-    public void handleCircleInput(Circle circle, String direction) {
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+    public void handleCircleInput(Circle circle) {
+        if (this.direction == "up") {
             circle.setPosY(circle.getPosY() + (circle.getSpeed() * Gdx.graphics.getDeltaTime()));
+            this.direction = null;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+        if (this.direction == "down") {
             circle.setPosY(circle.getPosY() - (circle.getSpeed() * Gdx.graphics.getDeltaTime()));
+            this.direction = null;
         }
+        
     }
     
-    public void handleTriangleInput(Triangle triangle, String direction) {
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+    public void handleTriangleInput(Triangle triangle) {
+        if (this.direction == "left") {
             //triangle.setPosX(triangle.getPosX() + (triangle.getSpeed() * Gdx.graphics.getDeltaTime()));
         	triangle.setX(-5);
+            this.direction = null;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+        if (this.direction == "right") {
             //triangle.setPosX(triangle.getPosX() - (triangle.getSpeed() * Gdx.graphics.getDeltaTime()));
         	triangle.setX(5);
+            this.direction = null;
         }
+        
     }
+
 
 }
