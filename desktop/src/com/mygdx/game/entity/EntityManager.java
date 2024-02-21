288d2c0a916c1bac31c5298aff154eed65e2571a
aliciathl <aliciatay003@gmail.com>
2024-02-21 11:53:32 +0800

    updated entitymanager

--- desktop/src/com/mygdx/game/entity/EntityManager.java
+++ desktop/src/com/mygdx/game/entity/EntityManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.mygdx.game.collision.CollisionManager;
+import com.mygdx.game.pcm.PlayerControlManager;
 
 public class EntityManager implements EntityManagement{
 	private List<Entity> entities;
@@ -23,9 +24,15 @@ public class EntityManager implements EntityManagement{
         }
     }
 
-    public void move() {
+    //public void move() {
+        //for (Entity entity : entities) {
+            //entity.move(); 
+        //}
+    //}
+    
+    public void moveEntities(PlayerControlManager pcm) {
         for (Entity entity : entities) {
-            entity.move(); 
+            pcm.handlingPlayerInput();
         }
     }
 
@@ -51,5 +58,11 @@ public class EntityManager implements EntityManagement{
     		entities.add(entity);
     	}
     }
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
